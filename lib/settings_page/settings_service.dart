9e303d9c7cbf46f0265560973a9ad331c112ea18
Roman Laptev <52125833+Carapacik@users.noreply.github.com>
2022-04-12 21:07:55 +0300

    [1.1.7] Save theme (#3)
    * Shared preferences
    * Clean code with new linter

--- lib/settings_page/settings_service.dart
+++ lib/settings_page/settings_service.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsService {
-  Future<ThemeMode> themeMode() async => ThemeMode.system;
+  static const _key = 'theme';
+
+  Future<ThemeMode> themeMode() async {
+    final sp = await SharedPreferences.getInstance();
+    return ThemeMode.values.elementAt(sp.getInt(_key) ?? 0);
+  }
 
   Future<void> updateThemeMode(ThemeMode theme) async {
-    // shared_preferences use for save
+    final sp = await SharedPreferences.getInstance();
+    await sp.setInt(_key, theme.index);
   }
 }
--- lib/settings_page/settings_view.dart
+++ lib/settings_page/settings_view.dart
@@ -11,29 +11,27 @@ class SettingsView extends StatelessWidget {
   final SettingsController controller;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: DropdownButton<ThemeMode>(
-          value: controller.themeMode,
-          onChanged: controller.updateThemeMode,
-          items: const [
-            DropdownMenuItem(
-              value: ThemeMode.system,
-              child: Text('System Theme'),
-            ),
-            DropdownMenuItem(
-              value: ThemeMode.light,
-              child: Text('Light Theme'),
-            ),
-            DropdownMenuItem(
-              value: ThemeMode.dark,
-              child: Text('Dark Theme'),
-            )
-          ],
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: DropdownButton<ThemeMode>(
+            value: controller.themeMode,
+            onChanged: controller.updateThemeMode,
+            items: const [
+              DropdownMenuItem(
+                value: ThemeMode.system,
+                child: Text('System Theme'),
+              ),
+              DropdownMenuItem(
+                value: ThemeMode.light,
+                child: Text('Light Theme'),
+              ),
+              DropdownMenuItem(
+                value: ThemeMode.dark,
+                child: Text('Dark Theme'),
+              )
+            ],
+          ),
         ),
-      ),
-    );
-  }
+      );
 }
